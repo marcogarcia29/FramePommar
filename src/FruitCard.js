06f4b6879c8cd696a9b259f678d75246b57e0b36
Marco <marco.silva.garcia2901@gmail.com>
2021-11-11 11:16:49 -0300

    att nova

---
+++ src/FruitCard.js
@@ -0,0 +1,17 @@
+
+const FruitCard = (props) => {
+    const formatPrice = (price) => {
+        return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price)
+    }
+    return (
+        <div className='itens' key={props.id}>
+            <img src={props.pictureUrl}></img>
+            <h3>{props.name}</h3>
+            <p >{formatPrice(props.unitPrice)}</p>
+        </div>
+
+
+    )
+}
+
+export default FruitCard;
--- src/data/fruit_menu.js
+++ src/data/fruit_menu.js
@@ -5,4 +5,10 @@ export const fruitMenu = [
     unitPrice: 7.50,
     pictureUrl:"https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/banana_caturra1-7e2b9a3f6b39b3309915220270001382-1024-1024.jpg"
     },
+    {
+        id: 2,
+        name: 'Banana',
+        unitPrice: 7.30,
+        pictureUrl:"https://d3ugyf2ht6aenh.cloudfront.net/stores/746/397/products/banana_caturra1-7e2b9a3f6b39b3309915220270001382-1024-1024.jpg"
+    },
 ];

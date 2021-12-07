5b8c1d07207c666595b2ed0d190ed42b40900c1d
Framework <marco.silva.garcia2901@gmail.com>
2021-12-07 10:16:11 -0300

    add new item

--- src/data/fruit_menu.js
+++ src/data/fruit_menu.js
@@ -13,4 +13,11 @@ export default [
     pictureUrl:"https://img.elo7.com.br/product/zoom/27DBE8F/essencia-maca-vermelha-100-ml-essencia-saboaria.jpg",
     amount: 0,
     },
+    {
+        id: 3,
+        name: 'Ferrari',
+        unitPrice: 6.20,
+        pictureUrl:"https://conteudo.imguol.com.br/c/entretenimento/14/2021/06/14/ferrari-j50-1623676322939_v2_450x337.jpg",
+        amount: 0,
+        },
 ];
